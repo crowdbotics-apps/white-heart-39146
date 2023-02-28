0506e228b54b41e23f9fefff6a9d8e88a574b9c5
Crowdbotics <team@crowdbotics.com>
2023-02-28 05:46:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-heart-39146",
+  url: "https://white-heart-39146.botics.co",
+  api: "https://white-heart-39146.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
